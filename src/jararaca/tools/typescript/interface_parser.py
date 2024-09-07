86ee517e4c2d0a0199b944a5a1ac2bf76310a21b
Lucas S <me@luscasleo.dev>
2024-09-07 15:20:27 -0300

    feat: Created Microservice application parser to Typescript Interface

---
+++ src/jararaca/tools/typescript/interface_parser.py
@@ -0,0 +1,362 @@
+import inspect
+from dataclasses import dataclass
+from decimal import Decimal
+from io import StringIO
+from types import NoneType, UnionType
+from typing import Annotated, Any, Literal, Type, TypeVar, get_origin
+from uuid import UUID
+
+from fastapi import Request, Response
+from fastapi.params import Body, Cookie, Depends, Header, Path, Query
+from pydantic import BaseModel
+
+from jararaca.microservice import Microservice
+from jararaca.presentation.decorators import HttpMapping, RestController
+
+
+def snake_to_camel(snake_str: str) -> str:
+    components = snake_str.split("_")
+    return components[0] + "".join(x.title() for x in components[1:])
+
+
+def get_field_type_for_ts(field_type: Any) -> Any:
+    if field_type == NoneType:
+        return "null"
+    if field_type == UUID:
+        return "string"
+    if field_type == str:
+        return "string"
+    if field_type == int:
+        return "number"
+    if field_type == float:
+        return "number"
+    if field_type == bool:
+        return "boolean"
+    if field_type == Decimal:
+        return "number"
+    if get_origin(field_type) == tuple:
+        return f"[{', '.join([get_field_type_for_ts(field) for field in field_type.__args__])}]"
+    if get_origin(field_type) == list:
+        return f"{get_field_type_for_ts(field_type.__args__[0])}[]"
+    if get_origin(field_type) == dict:
+        return f"{{[key: {get_field_type_for_ts(field_type.__args__[0])}]: {get_field_type_for_ts(field_type.__args__[1])}}}"
+    if inspect.isclass(field_type):
+        if not hasattr(field_type, "__pydantic_generic_metadata__"):
+            return field_type.__name__
+        pydantic_metadata = getattr(field_type, "__pydantic_generic_metadata__")
+
+        name = (
+            pydantic_metadata.get("origin").__name__
+            if pydantic_metadata.get("origin") is not None
+            else field_type.__name__
+        )
+        args = pydantic_metadata.get("args")
+
+        if len(args) > 0:
+            return "%s<%s>" % (
+                name,
+                ", ".join([get_field_type_for_ts(arg) for arg in args]),
+            )
+
+        return name
+
+    if hasattr(field_type, "__class__") and field_type.__class__ is TypeVar:
+        return field_type.__name__
+    if get_origin(field_type) == Literal:
+        return " | ".join([f'"{x}"' for x in field_type.__args__])
+    if get_origin(field_type) == UnionType:
+        return " | ".join([get_field_type_for_ts(x) for x in field_type.__args__])
+
+    return "unknown"
+
+
+def is_generic_type(field_type: Any) -> bool:
+    return hasattr(field_type, "__parameters__") and len(field_type.__parameters__) > 0
+
+
+def get_generic_args(field_type: Any) -> Any:
+    return field_type.__parameters__
+
+
+def parse_basemodel_to_typescript_interface(basemodel_type: Type[BaseModel]) -> str:
+    string_builder = StringIO()
+
+    if is_generic_type(basemodel_type):
+        string_builder.write(
+            f"export interface {basemodel_type.__name__}<{', '.join([arg.__name__ for arg in get_generic_args(basemodel_type)])}> {{\n"
+        )
+    else:
+        string_builder.write(f"interface {basemodel_type.__name__} {{\n")
+
+    for field_name, field in basemodel_type.__annotations__.items():
+        string_builder.write(
+            f"  {snake_to_camel(field_name)}: {get_field_type_for_ts(field)};\n"
+        )
+    string_builder.write("}\n")
+    return string_builder.getvalue()
+
+
+def write_basemodels_to_typescript(types: list[Any]) -> str:
+    buffer = StringIO()
+    for t in types:
+        buffer.write(parse_basemodel_to_typescript_interface(t))
+
+    return buffer.getvalue()
+
+
+def write_microservice_to_typescript_interface(
+    microservice: Microservice,
+) -> str:
+    rest_controller_buffer = StringIO()
+    mapped_types_set: set[Any] = set()
+
+    for controller in microservice.controllers:
+        rest_controller = RestController.get_controller(controller)
+        if rest_controller is None:
+            continue
+
+        controller_class_str, types = write_rest_controller_to_typescript_interface(
+            rest_controller, controller
+        )
+
+        mapped_types_set.update(types)
+        rest_controller_buffer.write(controller_class_str)
+
+    final_buffer = StringIO()
+
+    final_buffer.write(
+        """
+export interface HttpBackendRequest {
+  method: string;
+  path: string;
+  headers: { [key: string]: string };
+  query: { [key: string]: string };
+  body: any;
+}
+
+export interface HttpBackend {
+  request<T>(request: HttpBackendRequest): Promise<T>;
+}
+
+export abstract class HttpService {
+  constructor(protected readonly httpBackend: HttpBackend) {}
+}
+"""
+    )
+
+    for t in mapped_types_set:
+        if not is_primitive(t):
+            final_buffer.write(parse_basemodel_to_typescript_interface(t))
+
+    final_buffer.write(rest_controller_buffer.getvalue())
+
+    return final_buffer.getvalue()
+
+
+def is_primitive(field_type: Any) -> bool:
+    return field_type in [str, int, float, bool, NoneType, UUID, Decimal]
+
+
+def write_rest_controller_to_typescript_interface(
+    rest_controller: RestController, controller: type
+) -> tuple[str, set[Any]]:
+
+    class_buffer = StringIO()
+
+    class_buffer.write(
+        f"export abstract class {controller.__name__} extends HttpService {{\n"
+    )
+
+    mapped_types: set[Any] = set()
+
+    for name, member in inspect.getmembers(controller, predicate=inspect.isfunction):
+        if (mapping := HttpMapping.get_http_mapping(member)) is not None:
+            return_type = member.__annotations__.get("return")
+
+            if return_type is None:
+                return_type = NoneType
+
+            mapped_types.update(extract_all_envolved_types(return_type))
+
+            return_value_repr = get_field_type_for_ts(return_type)
+
+            arg_params_spec, parametes_mapped_types = extract_parameters(member)
+            mapped_types.update(parametes_mapped_types)
+
+            class_buffer.write(
+                f"\tasync {snake_to_camel(name)}({mount_parametes_arguments(arg_params_spec)}): Promise<{return_value_repr}> {{\n"
+            )
+
+            class_buffer.write("\t\t")
+            class_buffer.write(
+                "const response = await this.httpBackend.request<%s>({ \n"
+                % return_value_repr
+            )
+            class_buffer.write(f'\t\t\tmethod: "{mapping.method}",\n')
+
+            class_buffer.write(
+                f"\t\t\tpath: `{parse_path_with_params(mapping.path, arg_params_spec)}`,\n"
+            )
+
+            class_buffer.write("\t\t\theaders: {\n")
+            class_buffer.write("\t\t\t},\n")
+            class_buffer.write("\t\t\tquery: {\n")
+            class_buffer.write("\t\t\t},\n")
+            class_buffer.write("\t\t\tbody: {\n")
+            class_buffer.write("\t\t\t}\n")
+            class_buffer.write("\t\t});\n")
+            class_buffer.write("\t\treturn response;\n")
+
+            class_buffer.write("\t}\n")
+
+    class_buffer.write("}\n")
+
+    return class_buffer.getvalue(), mapped_types
+
+
+EXCLUDED_REQUESTS_TYPES = [Request, Response]
+
+
+@dataclass
+class HttpParemeterSpec:
+    type_: Literal["query", "path", "body", "header", "cookie"]
+    name: str
+    required: bool
+    argument_type_str: str
+
+
+def parse_path_with_params(path: str, parameters: list[HttpParemeterSpec]) -> str:
+    for parameter in parameters:
+        path = path.replace(f":{parameter.name}", f"${{{parameter.name}}}")
+    return path
+
+
+def mount_parametes_arguments(parameters: list[HttpParemeterSpec]) -> str:
+    return ", ".join(
+        [f"{parameter.name}: {parameter.argument_type_str}" for parameter in parameters]
+    )
+
+
+def extract_parameters(member: Any) -> tuple[list[HttpParemeterSpec], set[Any]]:
+    parameters_list: list[HttpParemeterSpec] = []
+    mapped_types: set[Any] = set()
+
+    for parameter_name, parameter_type in member.__annotations__.items():
+        if parameter_name == "return":
+            continue
+        if parameter_type in EXCLUDED_REQUESTS_TYPES:
+            continue
+
+        if get_origin(parameter_type) == Annotated:
+            annotated_type_hook = parameter_type.__metadata__[0]
+            if isinstance(annotated_type_hook, Header):
+                mapped_types.add(str)
+                parameters_list.append(
+                    HttpParemeterSpec(
+                        type_="header",
+                        name=parameter_name,
+                        required=True,
+                        argument_type_str=get_field_type_for_ts(str),
+                    )
+                )
+            elif isinstance(annotated_type_hook, Cookie):
+                mapped_types.add(str)
+                parameters_list.append(
+                    HttpParemeterSpec(
+                        type_="cookie",
+                        name=parameter_name,
+                        required=True,
+                        argument_type_str=get_field_type_for_ts(str),
+                    )
+                )
+            elif isinstance(annotated_type_hook, Body):
+                mapped_types.update(extract_all_envolved_types(parameter_type))
+                parameters_list.append(
+                    HttpParemeterSpec(
+                        type_="body",
+                        name=parameter_name,
+                        required=True,
+                        argument_type_str=get_field_type_for_ts(parameter_type),
+                    )
+                )
+            elif isinstance(annotated_type_hook, Query):
+                mapped_types.add(parameter_type)
+                parameters_list.append(
+                    HttpParemeterSpec(
+                        type_="query",
+                        name=parameter_name,
+                        required=True,
+                        argument_type_str=get_field_type_for_ts(parameter_type),
+                    )
+                )
+            elif isinstance(annotated_type_hook, Path):
+                mapped_types.add(parameter_type)
+                parameters_list.append(
+                    HttpParemeterSpec(
+                        type_="path",
+                        name=parameter_name,
+                        required=True,
+                        argument_type_str=get_field_type_for_ts(parameter_type),
+                    )
+                )
+            elif isinstance(annotated_type_hook, Depends):
+                depends_hook = annotated_type_hook.dependency
+                rec_parameters, rec_mapped_types = extract_parameters(depends_hook)
+                mapped_types.update(rec_mapped_types)
+                parameters_list.extend(rec_parameters)
+                # if callable(depends_hook):
+                #     depends_hook = depends_hook()
+                # parameters_list.append(
+                #     HttpParemeterSpec(
+                #         type_="query",
+                #         name=parameter_name,
+                #         required=True,
+                #         argument_type_str=get_field_type_for_ts(depends_hook),
+                #     )
+                # )
+        elif issubclass(parameter_type, BaseModel):
+            mapped_types.update(extract_all_envolved_types(parameter_type))
+            parameters_list.append(
+                HttpParemeterSpec(
+                    type_="body",
+                    name=parameter_name,
+                    required=True,
+                    argument_type_str=get_field_type_for_ts(parameter_type),
+                )
+            )
+        else:
+            mapped_types.add(parameter_type)
+            parameters_list.append(
+                HttpParemeterSpec(
+                    type_="query",
+                    name=parameter_name,
+                    required=True,
+                    argument_type_str=get_field_type_for_ts(parameter_type),
+                )
+            )
+
+    return parameters_list, mapped_types
+
+
+def extract_all_envolved_types(field_type: Any) -> set[Any]:
+    mapped_types: set[Any] = set()
+
+    if field_type is None:
+        return mapped_types
+
+    if inspect.isclass(field_type):
+        if hasattr(field_type, "__pydantic_generic_metadata__"):
+            metadata = getattr(field_type, "__pydantic_generic_metadata__")
+            if metadata.get("origin") is not None:
+                mapped_types.add(metadata.get("origin"))
+            else:
+                mapped_types.add(field_type)
+            mapped_types.update(metadata.get("args"))
+        else:
+            mapped_types.add(field_type)
+
+    if hasattr(field_type, "__args__"):
+        for arg in field_type.__args__:
+            mapped_types.update(extract_all_envolved_types(arg))
+
+    return mapped_types
