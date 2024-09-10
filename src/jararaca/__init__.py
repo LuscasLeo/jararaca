46cd720bbb60b5bb60b0823f51af0610d240310b
Lucas S <me@luscasleo.dev>
2024-09-09 22:59:49 -0300

    refactor: Add new modules and update imports in __init__.py

--- src/jararaca/__init__.py
+++ src/jararaca/__init__.py
@@ -1,14 +1,75 @@
 from .core.providers import ProviderSpec, Token
 from .di import Container
+from .messagebus import Message
+from .messagebus.decorators import IncomingHandler, MessageBusController
+from .messagebus.interceptors.publisher_interceptor import (
+    AIOPikaConnectionFactory,
+    MessageBusPublisherInterceptor,
+)
+from .messagebus.publisher import use_publisher
 from .messagebus.worker import MessageBusWorker
-from .microservice import Microservice
+from .microservice import Microservice, use_current_container
+from .persistence.base import (
+    T_BASEMODEL,
+    BaseEntity,
+    CriteriaBasedAttributeQueryInjector,
+    CRUDOperations,
+    DateCriteria,
+    DatedEntity,
+    DateOrderedFilter,
+    DateOrderedQueryInjector,
+    Identifiable,
+    IdentifiableEntity,
+    Paginated,
+    PaginatedFilter,
+    PaginatedQueryInjector,
+    QueryInjector,
+    QueryOperations,
+    StringCriteria,
+)
 from .persistence.interceptors.aiosqa_interceptor import (
     AIOSQAConfig,
     AIOSqlAlchemySessionInterceptor,
+    use_session,
 )
+from .presentation.decorators import Delete, Get, Patch, Post, Put, RestController
+from .presentation.http_microservice import HttpMicroservice
 from .presentation.server import create_http_server
+from .presentation.websocket.decorators import WebSocketEndpoint
+from .presentation.websocket.redis import RedisWebSocketConnectionBackend
+from .presentation.websocket.websocket_interceptor import (
+    WebSocketInterceptor,
+    use_ws_manager,
+)
+from .scheduler.decorators import ScheduledAction
+from .tools.app_config.interceptor import AppConfigurationInterceptor
 
 __all__ = [
+    "QueryInjector",
+    "HttpMicroservice",
+    "use_current_container",
+    "T_BASEMODEL",
+    "DatedEntity",
+    "BaseEntity",
+    "use_ws_manager",
+    "WebSocketEndpoint",
+    "CriteriaBasedAttributeQueryInjector",
+    "Identifiable",
+    "IdentifiableEntity",
+    "Message",
+    "StringCriteria",
+    "DateCriteria",
+    "DateOrderedFilter",
+    "DateOrderedQueryInjector",
+    "Paginated",
+    "PaginatedFilter",
+    "PaginatedQueryInjector",
+    "QueryOperations",
+    "CRUDOperations",
+    "RestController",
+    "MessageBusController",
+    "IncomingHandler",
+    "ScheduledAction",
     "Microservice",
     "ProviderSpec",
     "Token",
@@ -17,4 +78,16 @@ __all__ = [
     "create_http_server",
     "MessageBusWorker",
     "Container",
+    "WebSocketInterceptor",
+    "use_session",
+    "Post",
+    "Get",
+    "Patch",
+    "Put",
+    "Delete",
+    "use_publisher",
+    "AIOPikaConnectionFactory",
+    "MessageBusPublisherInterceptor",
+    "RedisWebSocketConnectionBackend",
+    "AppConfigurationInterceptor",
 ]
